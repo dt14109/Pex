e6177616bceba09f68c8bcbebfed8ca963748b97
dt14109 <44623205+dt14109@users.noreply.github.com>
2019-02-20 18:41:09 -0600

    Added some comments to make it clearer

--- opencvTry.cpp
+++ opencvTry.cpp
@@ -3,6 +3,8 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/videoio/legacy/constants_c.h>
 #include <fstream>
+#include <vector>
+#include <functional>
 
 using namespace std;
 using namespace cv;
@@ -20,20 +22,15 @@ using namespace cv;
 //If a frame timestamp(in seconds) is 3.14 and the dimensions are 3x3 an example line might look like :
 //
 //3.14, 42, 255, 9, 13, 67, 0, 27, 33, 123  // timestamp + 9 values (3x3) 
-
 int main()
 {
 	try 
 	{
 		ofstream ofs;
 		string file = "";
-		string video = "";
 		double timestamp = 0.0;
-		double frameRate = 0.0;
 		vector<uchar> eachFramePixelVector; // Each frame has a vector of pixels
-		//vector<int> medianVector;			// Vector of all medians
-		//vector<double> timestampVector;		// Vector of all time stamps
-		pair<int, double> pair1;
+		pair<int, double> pair1;			// Median, frame timestamp
 		vector<pair <int, double>> resultsVector;
 
 		while(0 == file.size())
@@ -62,36 +59,37 @@ int main()
 			return 0;
 		}
 
-		// Extract the frames to the vector
+		// Process each frame
 		for (int frameNum = 0; frameNum < videoCapture.get(CV_CAP_PROP_FRAME_COUNT); frameNum++)
 		{
 			Mat frame;
 			videoCapture >> frame; // get the next frame from video
 
-			// Is this the timestamp in milliseconds for each frame?
+			// timestamp in milliseconds for each frame
 			timestamp = videoCapture.get(CV_CAP_PROP_POS_MSEC);
-			//timestampVector.push_back(timestamp);
 
 			// Convert it to grayscale
 			Mat grayMat;
 			cv::cvtColor(frame, grayMat, COLOR_BGR2GRAY);
 
-			// This gets each grayscale pixel (320 x 240 = 76,800 pixels) for this frame
+			// Get each grayscale pixel and add them to the vector
 			for_each(grayMat.begin<uchar>(), grayMat.end<uchar>(), [&](uchar& pixel) {
 				eachFramePixelVector.push_back(pixel);
 			});
 
-			// Get the Median value and put it into the Vector of each Median
-			nth_element(eachFramePixelVector.begin(), eachFramePixelVector.begin() + eachFramePixelVector.size() / 2, eachFramePixelVector.end());
-			//medianVector.push_back(eachFramePixelVector[eachFramePixelVector.size() / 2]);
+			// Now that we have each pixel for this frame, we want to determine the Median for this frame.
+			// Thus we will display the Timestamp and the Median for each frame
 
-			pair1.first = eachFramePixelVector[eachFramePixelVector.size() / 2];
+			// Get the Median value and put it into the Vector of each Median
+			// It's more efficient to do the partial sort
+			nth_element(eachFramePixelVector.begin(), eachFramePixelVector.begin() + eachFramePixelVector.size() / 2, eachFramePixelVector.end());
+
+			pair1.first = eachFramePixelVector[eachFramePixelVector.size() / 2];  // Median
 			pair1.second = timestamp;
 			resultsVector.push_back(pair1);
 		}
 
-		// write the medians to a CSV file together with the timestamp of the frame.
-		// Copy each median to the file
+		// Write the medians to a CSV file together with the timestamp of the frame.
 		for_each(resultsVector.begin(), resultsVector.end(), [&](pair<int, double> pair1) {
 			ofs << pair1.second << ", " << pair1.first << ", ";
 		});
